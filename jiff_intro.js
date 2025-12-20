f3ee5f266af37da73fa26f596025bdcd6ccdd810
faint-0427 <70187232+faint-0427@users.noreply.github.com>
2025-12-20 18:19:35 +0900

    Add files via upload

---
+++ jiff_intro.js
@@ -0,0 +1,29 @@
+const poster_list = new Swiper('.poster_list', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true, // 중앙 정렬 필수
+  slidesPerView: 3, // CSS에서 정한 슬라이드 너비를 따름
+  speed: 600,
+  coverflowEffect: {
+    rotate: 30,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
+  watchSlidesProgress: true,
+  spaceBetween: 50, 
+  resistanceRatio: 0,
+   autoplay: {
+   delay: 3000,
+ },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  loop:true,
+    mousewheel: {
+    invert: false,
+  },
+  
+});
